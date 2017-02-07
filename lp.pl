4a950c953c476386498889b2c51a139710bbea66
undeadVivisector <mvandertramps@gmail.com>
2017-02-07 01:45:44 -0500

    a different method of adding th H's to the N's. For some reason it just writes down the operation

--- lp.pl
+++ lp.pl
@@ -1,6 +1,5 @@
-sum-up([],0).
-sum-up([H|T], N):-
+sum([],0).
+sum([H|T], N):-
       nl, write('H:'), write(H),
       nl, write('N:'), write(N), nl,
-      sum-up(T, K),
-      N is K+1.
+      sum(T, H+N).

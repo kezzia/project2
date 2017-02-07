2c61f0bc7a4773b760c8257a602134bcf0eb2659
undeadVivisector <mvandertramps@gmail.com>
2017-02-07 14:29:39 -0500

    realised i had the right answer for sum-up-numbers-general one commit ago. this function is now sum-up-numbers-general. added comments

--- lp.pl
+++ lp.pl
@@ -1,4 +1,4 @@
-sum([], 0).
-sum([H|T], N):-
-    sum(T, X),
-    N is X + H.
+sum_up_numbers_general([], 0). %if the list is empty, return True
+sum_up_numbers_general([H|T], N):- %otherwise
+    sum_up_numbers_general(T,N1), %call sum_up_numbers_general on the current sum and the rest of the list
+    N is N1+H. %N = current sum + new value

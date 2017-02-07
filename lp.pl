3215bc0a27ccc8bae828509febcd7f3091035668
undeadVivisector <mvandertramps@gmail.com>
2017-02-07 15:03:19 -0500

    sum_up_numbers_general can now ignore numbers

--- lp.pl
+++ lp.pl
@@ -1,4 +1,8 @@
-sum_up_numbers_general([], 0). %if the list is empty, return True
-sum_up_numbers_general([H|T], N):- %otherwise
-    sum_up_numbers_general(T,N1), %call sum_up_numbers_general on the current sum and the rest of the list
-    N is N1+H. %N = current sum + new value
+sum_up_numbers_general([],0).		%if the list is empty, return True
+sum_up_numbers_general([H|T],N):-	%otherwise
+    number(H),                          %if new value is numerical
+    sum_up_numbers_general(T,N1),	%carry on w current sum and rest of list
+    N is N1+H.                          %N = current sum + new value
+sum_up_numbers_general([H|T],N):-	%else
+    \+number(H),                        %if new value is non numerical
+    sum_up_numbers_general(T,N).	%ignore it

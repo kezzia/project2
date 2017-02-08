54320ddcadd3c9322ab918292f7897f8be5a8fbd
undeadVivisector <mvandertramps@gmail.com>
2017-02-07 19:40:01 -0500

    sum-up-numbers-simple can now add the numbers in the list. if it encounters a nested list it gets confused and fails

--- lp.pl
+++ lp.pl
@@ -1,4 +1,14 @@
-<<<<<<< HEAD
+%SUM UP NUMBERS SIMPLE
+sum_up_numbers_simple([], 0).       %if the list is empty, return True
+
+sum_up_numbers_simple([H|T], N):-   %otherwise
+    number(H),                      %if new value is numerical
+    sum_up_numbers_simple(T,N1),    %call pred on the current sum rest of the list
+    N is N1+H.                      %N = current sum + new value
+
+
+
+%SUM UP NUMBERS GENERAL
 sum_up_numbers_general([], 0).      %if the list is empty, return True
 
 sum_up_numbers_general([H|T],N):-   %otherwise,
@@ -8,28 +18,25 @@ sum_up_numbers_general([H|T],N):-   %otherwise,
     N is N1+H.
 
 sum_up_numbers_general([H|T],N):-   %otherwise,
-sum_up_numbers_general([], 0).		%if the list is empty, return True
+sum_up_numbers_general([], 0).      %if the list is empty, return True
 
 sum_up_numbers_general([H|T],N):-   %otherwise,
-    is_list(H),						%if new value is a nested list
-    length(H,1),					%and it has a length of 1
+    is_list(H),                     %if new value is a nested list
+    length(H,1),                    %and it has a length of 1
     sum_up_numbers_general(T,N1),
     N is N1+H.
 
 sum_up_numbers_general([H|T],N):-   %otherwise,
-    is_list(H),						%if new value is a nested list
-    \+length(H,1),					%and its length > 1
-    append(H,T,Z),					%flatten list by appending nested list to T
+    is_list(H),                     %if new value is a nested list
+    \+length(H,1),                  %and its length > 1
+    append(H,T,Z),                  %flatten list by appending nested list to T
     sum_up_numbers_general(Z,N).    %continue iterating through flattened list
 
-sum_up_numbers_general([H|T], N):-	%otherwise
-    number(H),						%if new value is numerical
-    sum_up_numbers_general(T,N1),	%call sum_up_numbers_general on the current sum and the rest of the list
-    N is N1+H.						%N = current sum + new value
+sum_up_numbers_general([H|T], N):-  %otherwise
+    number(H),                      %if new value is numerical
+    sum_up_numbers_general(T,N1),   %call pred on current sum and rest of list
+    N is N1+H.                      %N = current sum + new value
 
-sum_up_numbers_general([H|T], N):-	%else
-	\+number(H),					%if new value is non numerical
-    sum_up_numbers_general(T,N).	%ignore it
-
-
-sum_up_numbers_simple([], 0). %if the list is empty, return True
+sum_up_numbers_general([H|T], N):-  %else
+	\+number(H),                %if new value is non numerical
+    sum_up_numbers_general(T,N).    %ignore it
